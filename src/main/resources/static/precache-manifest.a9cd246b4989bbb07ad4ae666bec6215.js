ce6e968b2267f90ff68c9f3075e24281aa23c694
ShivamSharma15 <bestshivam15@gmail.com>
2021-08-08 23:51:31 +0530

    adding for Deplyoment

---
+++ src/main/resources/static/precache-manifest.a9cd246b4989bbb07ad4ae666bec6215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3f3ce616b7e96dc918a98b113733bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "846e4a055e0f04d16c25",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "846e4a055e0f04d16c25",
+    "url": "/static/js/main.ead48a29.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
